1e81240a2cff8b9efe4580a298cda9e8ee367f33
necati <necat@DESKTOP-T27LVSM>
2022-10-28 18:43:52 +0300

    2.commit (bugs)

--- src/oopWithNLayeredApp/Main.java
+++ src/oopWithNLayeredApp/Main.java
@@ -1,10 +1,7 @@
 package oopWithNLayeredApp;
 
 import oopWithNLayeredApp.business.ProductManager;
-import oopWithNLayeredApp.core.logging.DatabaseLogger;
-import oopWithNLayeredApp.core.logging.FileLogger;
 import oopWithNLayeredApp.core.logging.Logger;
-import oopWithNLayeredApp.core.logging.MailLogger;
 import oopWithNLayeredApp.dataAccess.JdbcProductDao;
 import oopWithNLayeredApp.entities.Product;
 
@@ -13,7 +10,7 @@ public class Main {
     public static void main(String[] args)throws Exception {
 
         Product product1 = new Product(1,"Telefon",11);
-        Logger[] loggers = {new DatabaseLogger(),new FileLogger(), new MailLogger()};
+        Logger[] loggers = {};
 
         ProductManager productManager=new ProductManager(new JdbcProductDao(),loggers);
         productManager.add(product1);
